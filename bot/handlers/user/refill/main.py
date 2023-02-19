8ac624d5cba4fa0eca5bbe204d7b750254bb7535
kemran <racom4789@gmail,com>
2023-02-19 17:07:01 +0300

    Refill

--- bot/handlers/user/refill/main.py
+++ bot/handlers/user/refill/main.py
@@ -1,11 +1,11 @@
 from asyncio import Lock
 from aiogram import Dispatcher
-from aiogram.types import Message, CallbackGame
+from aiogram.types import Message, CallbackQuery
 from aiogram.dispatcher import FSMContext
 from datetime import datetime, timezone
 
 import arrow
-
+from bot.env import *
 
 import bot.Database.methods.create as db_create
 import bot.Database.methods.get as db_select
@@ -16,5 +16,11 @@ import bot.keyboards.reply as reply
 lock = Lock()
 
 
+async def top_up(callback: CallbackQuery):
+    await bot.delete_message(
+
+    )
+
 def register_refill_handlers(dp:Dispatcher):
+
     pass
