67f84bc9191a5c8fd899a26c6421bcf5b24933f9
Tura <turapro3443@mail.ru>
2023-02-19 17:15:41 +0300

    update

--- bot/env.py
+++ bot/env.py
@@ -2,7 +2,8 @@ from aiogram import Bot
 
 
 bot = Bot(token='6019030878:AAFoaqn3lOrXbwnvhSD_-g4BLQGP6GDTkZs')
-PAIMANTS_TOKEN = '1744374395:TEST:eda4edc2e768372bd2e6'
+
+payment_token = '1744374395:TEST:eda4edc2e768372bd2e6'
 
 republics = [
     'Ингушетия', 'Северная Осетия — Алания', 'Дагестан', 'Чечня', 'Кабардино-Балкария',
