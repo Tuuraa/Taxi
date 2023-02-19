b9c012577c4e898ec020ee4207730f3f428f9551
kemran <racom4789@gmail,com>
2023-02-19 17:07:28 +0300

    Refill

--- bot/env.py
+++ bot/env.py
@@ -2,6 +2,7 @@ from aiogram import Bot
 
 
 bot = Bot(token='6019030878:AAFoaqn3lOrXbwnvhSD_-g4BLQGP6GDTkZs')
+PAIMANTS_TOKEN = '1744374395:TEST:eda4edc2e768372bd2e6'
 
 republics = [
     'Ингушетия', 'Северная Осетия — Алания', 'Дагестан', 'Чечня', 'Кабардино-Балкария',
