f7ff5fb2dfaf7f70af3b0b0807ed0f707da4abd2
Tura <turapro3443@mail.ru>
2023-03-27 23:47:05 +0300

    update

--- bot/env.py
+++ bot/env.py
@@ -1,16 +1,16 @@
 from aiogram import Bot
 
 
-bot = Bot(token='6019030878:AAFoaqn3lOrXbwnvhSD_-g4BLQGP6GDTkZs')
+bot = Bot(token='6188206507:AAH1du89pRXK3Hw3KXnlvDOYeU75X96tx5U')
 
-payment_token = '1744374395:TEST:eda4edc2e768372bd2e6'
+payment_token = '1902332405:LIVE:638151872098154178'
 
 republics = [
     'Ингушетия', 'Северная Осетия — Алания', 'Дагестан', 'Чечня', 'Кабардино-Балкария',
     'Карачаево-Черкесия', 'Кавказские Минеральные Воды'
 ]
 
-admins = [855151774]
+admins = [855151774, 326460830, 982616847]
 
 admin_link = '@bluabitch, @AaakkmmM'
 
